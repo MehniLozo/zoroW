6628fbf2d12a77c52b919df4eb98f8e8594c5af9
mehni <lozomehni28@gmail.com>
2022-01-25 20:04:58 +0100

    first audio writing

--- cogs/commands/fun.py
+++ cogs/commands/fun.py
@@ -1,7 +1,7 @@
 import disnake,asyncio,json,os
 import requests
 import random
-from disnake import Embed
+from disnake import Embed,VoiceClient
 from disnake.ext import commands
 from disnake.ext.commands import Context
 from pathlib import Path
@@ -29,6 +29,56 @@ class Fun(commands.Cog,name = "fun-command"):
             print(e)
             print("*****************")
             await context.send("Sorry somthing in the cloud went wrong")
+            ############Audio stuff###########
+    @commands.command(
+            name="play"
+            )
+    async def play(self,context:Context):
+        youtube_dl.utils.bug_reports_message = lambda: ""
 
+        ytdl_format_options = {
+            "format": "bestaudio/best",
+            "outtmpl": "%(extractor)s-%(id)s-%(title)s.%(ext)s",
+            "restrictfilenames": True,
+            "noplaylist": True,
+            "nocheckcertificate": True,
+            "ignoreerrors": False,
+            "logtostderr": False,
+            "quiet": True,
+            "no_warnings": True,
+            "default_search": "auto",
+            "source_address": "0.0.0.0",  # bind to ipv4 since ipv6 addresses cause issues sometimes
+        }
+        ffmpeg_options = {"options":"-vn"}
+        ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+
+        class YTDLSource(disnake.PCMVolumeTransformer):
+            def __init__(self, source, *, data, volume=0.5):
+                super().__init__(source, volume)
+
+                self.data = data
+
+                self.title = data.get("title")
+                self.url = data.get("url")
+
+            @classmethod
+            async def from_url(cls, url, *, loop=None, stream=False):
+                loop = loop or asyncio.get_event_loop()
+                data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+                assert data
+
+                if "entries" in data:
+                    # take first item from a playlist
+                    data = data["entries"][0]
+
+                filename = data["url"] if stream else ytdl.prepare_filename(data)
+                assert filename
+
+                return cls(disnake.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
+
+
+        
 def setup(bot):
     bot.add_cog(Fun(bot))
